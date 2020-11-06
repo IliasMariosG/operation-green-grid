5f207969e63896655f517369dc029867aa04b30b
John Baxter <john.g.baxter@googlemail.com>
2020-11-06 19:24:21 +0000

    Add new timestamp to time_date_log

---
+++ commit_bot.py
@@ -0,0 +1,14 @@
+import os
+import datetime
+import time
+
+timestamp = datetime.datetime.now()
+os.system("echo '%s' >> time_date_log.txt" % timestamp)
+time.sleep(1)
+os.system("git add .")
+time.sleep(1)
+os.system("git commit -m 'Add new timestamp to time_date_log'")
+time.sleep(1)
+os.system("git push")
+
+
