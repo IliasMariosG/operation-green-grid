a6ab5fa6a5c2b3c71160113f127677bb6267201a
John Baxter <john.g.baxter@googlemail.com>
2020-11-06 19:45:56 +0000

    Add new timestamp to time_date_log

--- commit_bot.py
+++ commit_bot.py
@@ -1,14 +1,15 @@
 import os
 import datetime
 import time
+import random
 
-timestamp = datetime.datetime.now()
-os.system("echo '%s' >> time_date_log.txt" % timestamp)
-# time.sleep(1)
-os.system("git add .")
-# time.sleep(1)
-os.system("git commit -m 'Add new timestamp to time_date_log'")
-# time.sleep(1)
-os.system("git push")
+start_time = datetime.datetime.now()
+while 1:
+  timestamp = datetime.datetime.now()
+  os.system("echo '%s' >> time_date_log.txt" % timestamp)
+  os.system("git add .")
+  os.system("git commit -m 'Add new timestamp to time_date_log'")
+  os.system("git push")
+  time.sleep(random.randint(60))
 
 
